381b5297f5a658b1f866a1c8e0109fac8a48f60c
{hanok} <{hoxey2@gmail.com}>
2022-11-07 20:50:05 +0900

    codingTest

---
+++ src/main/java/codingtest/T10171.java
@@ -0,0 +1,11 @@
+package codingtest;
+
+public class T10171 { //고양이
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
---
+++ src/main/java/codingtest/T25093.java
@@ -0,0 +1,13 @@
+package codingtest;
+
+public class T25093 { //새싹
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
+
